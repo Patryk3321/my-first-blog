b2f29fe500bccc5a78d3240eeb5573436276bb86
Patryk <w.patryk@o2.pl>
2018-09-20 07:38:57 +0200

    Dodanie widoku do edytowania i dodawania postów na stronie

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,9 @@
-from django import forms
+
 
 from .models import Post
-
+from django import forms
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title','text',)
